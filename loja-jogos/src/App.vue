7f7909ee1a90c32a64ab31573091fee096476001
fdutrac <fabiohenrique@gestaointeligentesofware.com.br>
2021-05-29 03:51:14 -0300

    Finaliza Header

--- loja-jogos/src/App.vue
+++ loja-jogos/src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <div id="app">
-
+      <Header />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      teste
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
 
 export default {
   name: 'App',
   components: {
-
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/main.css';
+
 </style>
